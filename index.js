8905b507f8c73ce7a20925ba2b1904597621644b
abigailojeda <abigail.arte@gmail.com>
2023-01-08 16:27:43 +0000

    authentification

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+//express server
+const app = express();
+
+//ddbb
+dbConnection();
+
+//public directory
+app.use(express.static('public'));
+
+//CORS
+app.use(cors());
+
+//body parser
+app.use(express.json());
+
+//routes
+app.use('/api/auth', require('./routes/auth'))  
+
+//port
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`);
+});
+
